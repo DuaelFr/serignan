be0bb715641d631b44de8527027df41be8144c9a
Edouard Cunibil <admin@duael.fr>
2012-05-29 02:50:48 +0200

    update theme : add current page in breadcrumb

--- sites/default/themes/serignan/template.php
+++ sites/default/themes/serignan/template.php
@@ -10,3 +10,23 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+/**
+ * theme_breadcrumb
+ */
+function serignan_breadcrumb($vars) {
+  if (!drupal_is_front_page()) {
+    $end = end($vars['crumbs_breadcrumb_items']);
+    $vars['breadcrumb'][] = '<span>' . $end['link_title'] . '</span>';
+  }
+
+  $breadcrumb = $vars['breadcrumb'];
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
