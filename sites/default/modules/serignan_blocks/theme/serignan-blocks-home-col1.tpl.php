3166a7f7e70d6b5db2360e417dad10b126a5916a
Pycna Front <contact@pycna.com>
2012-09-25 14:05:00 +0200

    up module serignan_blocks : update from preprod

--- sites/default/modules/serignan_blocks/theme/serignan-blocks-home-col1.tpl.php
+++ sites/default/modules/serignan_blocks/theme/serignan-blocks-home-col1.tpl.php
@@ -1,3 +1,3 @@
 <a href="/mairie-services/demarches-administratives/demarches-en-mairie" title="Démarches administratives">
-  <img src="<?php echo $theme_path; ?>/images/home-col1.png" alt="Démarches administratives" />
+  <img src="/<?php echo $theme_path; ?>/images/home-col1.png" alt="Démarches administratives" />
 </a>
--- sites/default/modules/serignan_blocks/theme/serignan-blocks-sidebar-links.tpl.php
+++ sites/default/modules/serignan_blocks/theme/serignan-blocks-sidebar-links.tpl.php
@@ -1,5 +1,5 @@
 <ul>
-  <li class="registries"><a href="/sortir-decouvrir/annuaire">Annuaires de Sérignan</a></li>
+  <li class="registries"><a href="/sortir-decouvrir/annuaires">Annuaires de Sérignan</a></li>
   <li class="associations"><a href="/associations">Associations</a></li>
   <li class="proceedings"><a href="/mairie-services/demarches-administratives/demarches-en-mairie">Démarches administratives</a></li>
 </ul>
