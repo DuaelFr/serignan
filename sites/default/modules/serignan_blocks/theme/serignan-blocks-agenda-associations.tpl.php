46eb8d6b899a6282861146136156d9366e7f5874
Edouard Cunibil <admin@duael.fr>
2012-09-26 13:20:23 +0200

    up mod serignan_blocks : add agenda associations block

---
+++ sites/default/modules/serignan_blocks/theme/serignan-blocks-agenda-associations.tpl.php
@@ -0,0 +1,4 @@
+<div class="messages info">
+  <p>Retrouvez tous les événements organisés par nos associations sur la page de l'<a href="/actualites-agenda/agenda/associations">Agenda des associations</a>.</p>
+  <p>A MODIFIER dans <?php echo __FILE__; ?></p>
+</div>
