30113a056fb5ee98f4323256706b2ca1467f2e65
Pycna Front <contact@pycna.com>
2012-11-14 17:39:01 +0100

    up theme : change current magazine

--- sites/default/modules/serignan_blocks/theme/serignan-blocks-magazine.tpl.php
+++ sites/default/modules/serignan_blocks/theme/serignan-blocks-magazine.tpl.php
@@ -1,8 +1,8 @@
 <div class="magazine-block">
-  <img src="/<?php echo $theme_path; ?>/magazines/images/cover-2012-09.jpg" alt="Couverture magazine" />
+  <img src="/<?php echo $theme_path; ?>/magazines/images/cover-2012-11.jpg" alt="Couverture magazine" />
   <div class="content">
-    <h3>Septembre / Octobre 2012 - N°48</h3>
-    <p>Le temps des grands travaux</p>
-    <a href="http://fr.calameo.com/read/0011801944c378dd732fd" target="_blank">Lire le magazine</a>
+    <h3>Novembre / Décembre 2012 - N°49</h3>
+    <p>Dossier sécurité, des mesures et des actes</p>
+    <a href="http://www.calameo.com/read/001180194cf21b5c002fc" target="_blank">Lire le magazine</a>
   </div>
 </div>
